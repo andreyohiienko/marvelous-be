7d1de1e48fde9cee66ca295b15f1ba0e84113642
Andrii Ohiienko <andreyohiienko@gmail.com>
2023-06-10 17:30:43 -0700

    Add todo-list resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoListModule } from './todo-list/todo-list.module';
 
 @Module({
-  imports: [],
+  imports: [TodoListModule],
   controllers: [AppController],
   providers: [AppService],
 })
